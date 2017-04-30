810322e6a564418756b752f9f0e5c270b0d94d07
Miroslav Vuchevski <miroslav.vucevski@students.finki.ukim.mk>
2017-04-30 19:54:06 +0200

    Add files via upload
    First commit

---
+++ LemonadeB_1/LemonadeB_1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LemonadeB_1.Properties;
+
+namespace LemonadeB_1
+{
+    public partial class LemonadeBusiness : Form
+    {
+        Scene scene;
+        Bitmap background;
+        Point backgroundLocation;
+        int Day;
+
+        public LemonadeBusiness()
+        {
+            InitializeComponent();
+            scene = new Scene();       
+            background = new Bitmap(Resources.background);
+            backgroundLocation = new Point(0,0);
+            this.DoubleBuffered = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (NonBuyer nb in scene.nonbuyersNow) {
+                nb.Move();
+            }
+            foreach (Buyer nb in scene.buyersNow)
+            {
+                nb.Move();
+            }
+            Invalidate();
+        }
+
+        private void newNonBuyerTimer_Tick(object sender, EventArgs e)
+        {
+            scene.addNewNonByer();
+            scene.addNewByer();
+            foreach (Buyer b in scene.buyersNow)
+            {
+                b.waitingToBuy();
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(background,backgroundLocation);
+            scene.DrawNonByers(e.Graphics);
+            scene.DrawByers(e.Graphics);
+        }
+
+        private void addNonBuyers(int n) {
+            scene.addNonByers(n);
+            scene.addNewNonByer();
+        }
+
+        private void addBuyers(int n)
+        {
+            scene.addByers(n);
+            scene.addNewByer();
+        }
+
+        private void btStartDay_Click(object sender, EventArgs e)
+        {
+            btStartDay.Enabled = false;
+            gameTimer.Enabled = true;
+            newHumanTimer.Enabled = true;
+            dayTimer.Enabled = true;
+            addNonBuyers(5);
+            addBuyers(3);
+        }
+
+        private void dayTimer_Tick(object sender, EventArgs e)
+        {
+            Day++;
+            if (Day == 25) {
+                dayTimer.Enabled = false;
+                btStartDay.Enabled = true;
+                gameTimer.Enabled = false;
+                newHumanTimer.Enabled = false;
+                scene = new Scene();
+                Day = 0;
+            }
+        }
+    }
+}
---
+++ LemonadeB_1/LemonadeB_1/Human.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace LemonadeB_1
+{
+    public abstract class Human
+    {
+        public Point Position { get; set; }
+        public Bitmap Picture { get; set; }
+        public bool Buyer { get; set; }
+        public int type { get; set; }
+        public Point EndP { get; set; }
+        public Point changeDir { get; set; }
+        public bool Alive { get; set; }
+        public readonly int moveSpeed=1;
+
+        public Human(Point _Position, Bitmap _Picture, bool _Buyer, Point _EndP, int _type)
+        {
+            Position = _Position;
+            Picture = _Picture;
+            Buyer = _Buyer;
+            EndP = _EndP;
+            type = _type;
+            Alive = true;
+        }
+
+        public abstract void Draw(Graphics g);
+        public abstract void Move();
+    }
+}
